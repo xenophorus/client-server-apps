38bd49647174136c59211c0aef9320fe02eea99a
xenophorus <xenophorus@gmail.com>
2020-06-22 01:20:43 +1000

    1

--- less3/client.py
+++ less3/client.py
@@ -1,11 +1,24 @@
-from socket import SOCK_STREAM, socket
+# from socket import SOCK_STREAM, socket
 
-sock = socket(type=SOCK_STREAM)
-msg = 'Hello, world!'.encode('utf-8')
-sock.connect(('127.0.0.1', 9090))
-sock.send(msg)
+# sock = socket(type=SOCK_STREAM)
+# msg = 'Hello, world!'.encode('utf-8')
+# sock.connect(('192.168.1.42', 9090))
+# sock.send(msg)
+#
+# data = sock.recv(1024)
+# sock.close()
+#
+# print(data.decode('utf-8'))
 
-data = sock.recv(1024)
-sock.close()
+from socket import SOCK_DGRAM, socket
 
-print(data.decode('utf-8'))
+sock = socket(type=SOCK_DGRAM)
+
+try:
+    while True:
+        data = 'Hello, server!'
+        sock.sendto(data.encode('utf-8'), ('localhost', 9090))
+        msg, addr = sock.recvfrom(1024)
+        print(msg.decode('utf-8'))
+finally:
+    sock.close()
--- less3/server.py
+++ less3/server.py
@@ -1,17 +1,32 @@
-from socket import SOCK_STREAM, socket
+# from socket import SOCK_STREAM, socket
+#
+# sock = socket(type=SOCK_STREAM)
+# sock.bind(('192.168.1.67', 9090))
+# sock.listen(5)
+# conn, addr = sock.accept()
+#
+# print(f'Connection established {addr}')
+#
+# try:
+#     while True:
+#         data = conn.recv(1024)
+#         if not data:
+#             break
+#         conn.send(data.upper())
+# finally:
+#     conn.close()
 
-sock = socket(type=SOCK_STREAM)
+from socket import SOCK_DGRAM, socket
+
+sock = socket(type=SOCK_DGRAM)
 sock.bind(('127.0.0.1', 9090))
-sock.listen(5)
-conn, addr = sock.accept()
-
-print(f'Connection established {addr}')
 
 try:
     while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.send(data.upper())
+        msg, addr = sock.recvfrom(1024)
+        print(msg.decode('utf-8'))
+        ans = 'Hello, client!'
+        sock.sendto(ans.encode('utf-8'), addr)
+
 finally:
-    conn.close()
+    sock.close()
